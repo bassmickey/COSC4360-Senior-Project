b4b4c373c5a0c5f76548bea9884c285eeee4ed7f
Miguel Barajas <bassmickey1@gmail.com>
2022-01-25 13:48:12 -0600

    Initial Commit

---
+++ COSC4360 Senior Project/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COSC4360 Senior Project
+//
+//  Created by Miguel Barajas on 1/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
