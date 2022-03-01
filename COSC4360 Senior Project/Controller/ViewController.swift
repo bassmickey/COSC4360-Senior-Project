9b04755e2873edbff8c57afdd5deff7645dceed3
Miguel Barajas <bassmickey1@gmail.com>
2022-03-01 14:22:17 -0600

    Implemented the Twitter API, Swifter Framework, and printed the JSON file to the console

--- COSC4360 Senior Project/Controller/ViewController.swift
+++ COSC4360 Senior Project/Controller/ViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let swifter = Swifter(consumerKey: "", consumerSecret: "")
+        
+        swifter.searchTweet(using: "@Apple") { results, searchMetadata in
+            print(results)
+        } failure: { error in
+            print("Failed the fetch data from Twitter API, \(error)")
+        }
+
     }
 
 
