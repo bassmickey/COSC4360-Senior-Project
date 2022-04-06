7f33bbd1fbfc08c4b02e5750f6cba4ccd006cdcc
Miguel Barajas <bassmickey1@gmail.com>
2022-04-06 00:47:40 -0500

    Sentiment Score Computed sentiment score created functionality for the sense button created functionality for the text field

--- COSC4360 Senior Project/Controller/ViewController.swift
+++ COSC4360 Senior Project/Controller/ViewController.swift
@@ -11,38 +11,58 @@ import CoreML
 import SwiftyJSON
 
 class ViewController: UIViewController {
-    let sentimentClassifer = TweetSentimentClassifer()
+    @IBOutlet weak var textField: UITextField!
+    
+    let sentimentClassifier: TweetSentimentClassifer = {
+        do {
+            return try TweetSentimentClassifer(configuration: MLModelConfiguration())
+        } catch {
+            print(error)
+            fatalError()
+        }
+    }()
     
     let swifter = Swifter(consumerKey: "", consumerSecret: "")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        swifter.searchTweet(using: "#AMZN", lang: "en", count: 100, tweetMode: .extended) { results, searchMetadata in
-            
-            var tweets = [TweetSentimentClassiferInput]()
-            
-            for i in 0..<100 {
-                if let tweet = results[i]["full_text"].string {
-                    let tweetForClassification = TweetSentimentClassiferInput(text: tweet)
-                    tweets.append(tweetForClassification)
-                }
-            }
-            
-            do {
-                let predictions = try self.sentimentClassifer.predictions(inputs: tweets)
+    }
+    
+    @IBAction func sensePressed(_ sender: UIButton) {
+        if let searchText = textField.text {
+            swifter.searchTweet(using: searchText, lang: "en", count: 100, tweetMode: .extended) { results, searchMetadata in
                 
-                for prediction in predictions {
-                    print(prediction.label)
+                var tweets = [TweetSentimentClassiferInput]()
+                
+                for i in 0..<100 {
+                    if let tweet = results[i]["full_text"].string {
+                        let tweetForClassification = TweetSentimentClassiferInput(text: tweet)
+                        tweets.append(tweetForClassification)
+                    }
                 }
                 
-            } catch {
-                print("There was an error with making a prediction \(error)")
+                do {
+                    let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+                    var sentimentScore = 0
+                    
+                    for prediction in predictions {
+                        let sentiment = prediction.label
+                        if sentiment == "Pos" {sentimentScore += 1}
+                        else if sentiment == "Neg" {sentimentScore -= 1}
+                    }
+                    
+                    print(sentimentScore)
+                    
+                } catch {
+                    print("There was an error with making a prediction \(error)")
+                }
+                
+            } failure: { error in
+                print("Failed the fetch data from Twitter API, \(error)")
             }
-            
-        } failure: { error in
-            print("Failed the fetch data from Twitter API, \(error)")
         }
     }
+
 }
 
